111fffdffed8fc7eefe8fb232c2f7b4096d897ee
BraulioMercado <43398760+BraulioMercado@users.noreply.github.com>
2020-02-04 19:55:39 -0800

    Add files via upload

---
+++ ejemplo colecciones/ejemplo colecciones/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo_colecciones
+{
+    public class Coleccion:CollectionBase
+    {
+        public void Add(Object item) //agregar
+        {
+            InnerList.Add(item);
+        }
+        public void Remove(object item) //quitar
+        {
+            InnerList.Remove(item);
+        }
+        public new void Clear() //limpiar
+        {
+            InnerList.Clear();
+        }
+        public new int Count() //contar cuantos valores se tienen.
+        {
+            return InnerList.Count;
+        }
+    }
+}
